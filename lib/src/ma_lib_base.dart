029f2c419a86b58be164aa83484409605c1e7f7a
denis <payetdenis@gmail.com>
2025-08-21 15:04:51 +0400

    v2

--- lib/src/ma_lib_base.dart
+++ lib/src/ma_lib_base.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void writeLine(Object? obj){ //affichage dans la console, terminée par saut de ligne
-  print(obj);
+  print("> $obj <" );
 }
 
 void write(Object? obj){ //affichage dans la console, sans saut de ligne à la fin
